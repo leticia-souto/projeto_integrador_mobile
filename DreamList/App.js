d97c1fd5b0fc7ca6f052cc1544cae04560d6ef46
21042009-pa <paula.gianotto@aluno.senai.br>
2026-08-27 15:22:54 -0300

    metade do código

--- DreamList/App.js
+++ DreamList/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ListaDesejosScreen from './src/screens/ListaDesejosScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <ListaDesejosScreen />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
--- DreamList/src/screens/ListaDesejosScreen.js
+++ DreamList/src/screens/ListaDesejosScreen.js
@@ -16,5 +16,50 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const CHAVE_STORAGE = "DreamList: desejos";
 
 export default function ListaDesejosScreen(){
-    
+    const [desejo, setDesejo] = useState([])
+    const [textoInput, setTextoInput] = useState("");
+    const [carregando, setCarregando] = useState(true);
+
+    useEffect(() => {
+        async function carregarDesejos() {
+      try {
+        const desejosSalvos = await AsyncStorage.getItem(CHAVE_STORAGE);
+        if (desejosSalvos !== null) {
+          setDesejo(JSON.parse(desejosSalvos));
+        }
+      } catch (erro) {
+        console.error("Erro ao carregar desejos do storage:", erro);
+      } finally {
+        setCarregando(false);
+      }
+    }
+    carregarDesejos();
+    }, []);
+
+    useEffect(() => {
+    if (carregando) return;
+
+    AsyncStorage.setItem(CHAVE_STORAGE, JSON.stringify(desejo)).catch(
+      (erro) => {
+        console.error("Erro ao salvar desejo no storage: ", erro);
+      },
+    );
+  }, [desejo, carregando]);
+
+  function adicionarDesejo() {
+    const texto = textoInput.trim();
+
+    if (texto === "") return;
+
+    const novoDesejo = {
+      id: Date.now().toString(),
+      texto,
+      concluida: false,
+    };
+
+    setDesejo((desejoAtuais) => [...desejoAtuais, novoDesejo]);
+
+    setTextoInput("");
+  }
+
 }
