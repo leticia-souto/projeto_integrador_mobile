ab419949176242482fa9bdba11caa949770ceb02
21042009-pa <paula.gianotto@aluno.senai.br>
2026-09-03 13:44:21 -0300

    estilização

--- DreamList/src/components/DesejoItem.js
+++ DreamList/src/components/DesejoItem.js
@@ -31,27 +31,53 @@ export default function DesejoItem({ desejo, aoAlternarConcluida, aoExcluir, aoE
 
 const styles = StyleSheet.create({
   item: {
-
+    backgroundColor: "#FFFFFF", 
+    borderRadius: 18, 
+    padding: 16, 
+    marginBottom: 12, 
+    flexDirection: "row", 
+    alignItems: "center", 
+    shadowColor: "#3F3852", 
+    shadowOffset: { width: 0, height: 3, }, 
+    shadowOpacity: 0.08, 
+    shadowRadius: 8, 
+    elevation: 3,
   },
   textoContainer: {
-
+    flex: 1, 
+    paddingRight: 10,
   },
   texto: {
-
+    flex: 1, 
+    color: "#3F3852", 
+    fontSize: 16, 
+    fontWeight: "600", 
+    lineHeight: 22,
   },
   textoConcluido: {
-
+    color: "#9A94A5", 
+    textDecorationLine: "line-through",
   },
   botaoExcluir: {
-
+    backgroundColor: "#E98B9B", 
+    paddingHorizontal: 12, 
+    paddingVertical: 9, 
+    borderRadius: 11,
   },
   textoBotaoExcluir: {
-
+    color: "#FFFFFF", 
+    fontSize: 12, 
+    fontWeight: "700",
   },
   botaoEditar: {
-
+    backgroundColor: "#C9B8F4", 
+    paddingHorizontal: 12, 
+    paddingVertical: 9, 
+    borderRadius: 11,
   },
   textoBotaoEditar: {
-
+    color: "#3F3852", 
+    fontSize: 12, 
+    fontWeight: "700",
   },
 })
